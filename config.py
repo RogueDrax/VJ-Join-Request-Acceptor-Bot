54f305e6339fa269c0c84f016c0b0a51d9180713
RogueDrax <rogue.draxx@gmail.com>
2025-01-14 13:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "24435985"))
+API_HASH = environ.get("API_HASH", "0fec896446625478537e43906a4829f8")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002417235621"))
+ADMINS = int(environ.get("ADMINS", "5552335015"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://Drax:Roronoa.36@cluster0.cbsty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
